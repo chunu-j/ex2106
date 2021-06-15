1132bbd436d523dd9d9289a2321b091d8df4c070
chunu-j <61304585+chunu-j@users.noreply.github.com>
2021-06-15 21:47:18 +0900

    Initial commit ------------- '코드로 배우는 스프링 부트 웹 프로젝트' 무작정 1회독 하기 Part1의 chapter01 까지 따라함.

---
+++ src/main/java/org/zerock/ex2106/Ex2106Application.java
@@ -0,0 +1,13 @@
+package org.zerock.ex2106;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex2106Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex2106Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/ex2106/Ex2106ApplicationTests.java
@@ -0,0 +1,14 @@
+package org.zerock.ex2106;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex2106ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("contextLoad...");
+    }
+
+}
