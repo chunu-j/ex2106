3defd1b857a63c4ba9c5695376f0c213503efc37
chunu-j <61304585+chunu-j@users.noreply.github.com>
2021-06-19 16:52:17 +0900

    Part2의 chapter04_5_2 까지 따라함.
    서비스 계층에서 목록 처리하는 방법이 헷갈려 완벽하게 이해 후 넘어가기 위해 멈춤.

--- src/main/java/org/zerock/ex2106/service/GuestbookServiceImpl.java
+++ src/main/java/org/zerock/ex2106/service/GuestbookServiceImpl.java
@@ -2,10 +2,18 @@ package org.zerock.ex2106.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.zerock.ex2106.dto.GuestbookDTO;
+import org.zerock.ex2106.dto.PageRequestDTO;
+import org.zerock.ex2106.dto.PageResultDTO;
 import org.zerock.ex2106.entitiy.Guestbook;
 import org.zerock.ex2106.repository.GuestbookRepository;
+
+import java.util.function.Function;
+
 
 @Service
 @Log4j2
@@ -26,5 +34,14 @@ public class GuestbookServiceImpl implements GuestbookService{
         repository.save(entity);
 
         return entity.getGno();
+    }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+        Page<Guestbook> result = repository.findAll(pageable);
+        Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
+
+        return new PageResultDTO<>(result, fn);
     }
 }
--- src/test/java/org/zerock/ex2106/service/GuestbookServiceTest.java
+++ src/test/java/org/zerock/ex2106/service/GuestbookServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.ex2106.dto.GuestbookDTO;
+import org.zerock.ex2106.dto.PageRequestDTO;
+import org.zerock.ex2106.dto.PageResultDTO;
 import org.zerock.ex2106.entitiy.Guestbook;
 
 @SpringBootTest
@@ -11,6 +13,17 @@ public class GuestbookServiceTest {
 
     @Autowired
     private GuestbookService service;
+
+    @Test
+    public void testList() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+    }
 
     @Test
     public void testRegister() {
